860eccfa01823c60e8c150b30847bbdd38cc194d
Uri Nativ <uri.nativ@klarna.com>
2011-11-07 23:57:17 +0200

    Recreated with Rails 3.0.9

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'e739a85675f815ba703dbc2f1c29cb0ec3d4bf224118a51b66d2c97c2117d4583fc6ddd5287feea647e341580df58ccba4e6c7877c2086e078d56ae6aad836f3'
+DemoApp::Application.config.secret_token = '00b3cabb11c9249bcf3ed025bbe053f06190936ea47a184e7c4886687a671095c041c3293d85dc9d2277eb9e6aa29d546a77cdd080add0e7f56bd9046b0dd84c'
